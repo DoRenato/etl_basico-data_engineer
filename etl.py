4394a7881c3d53b4bb02eec3ba6ff7c8d0916b86
DoRenato <64191958+DoRenato@users.noreply.github.com>
2024-01-15 04:55:08 -0300

    program finished and python notebook add

--- etl.py
+++ etl.py
@@ -1,16 +1,28 @@
 import pandas as pd
 from datetime import datetime
 
-# abre uma planilha onde lê os dados dos clientes na primeira página.
-data_clients = pd.read_excel('client_data_test.xlsx', sheet_name='Página1')
+## Abre uma planilha onde lê os dados dos clientes contidos na primeira página da mesma.
+df_clients = pd.read_excel('dados/client_data_test.xlsx', sheet_name='Página1')
 
-# renomeia a coluna para um formato mais comum
-data_clients.rename(columns={'Data de Nascimento': 'Data_Nascimento'}, inplace=True) # o implace serve para salvar na direto no dataframe atual sem precisar armazenar em outra variavel
+## Renomeando as colunas para um formato padronizado: titulos das colunas em 
+#  minusculo e sem espaços.
+columns_renamed=[]
+for column in df_clients:
+    column_title=column.lower()
+    column_title=column_title.replace(' ','_')
+    columns_renamed.append(column_title)
+df_clients.columns = columns_renamed
 
-# pega somente uma coluna específica
-date_birth = data_clients['Data_Nascimento']
+## Agora será pego somente a coluna 'data_de_nascimento' para fazer o tratamento
+#  da idade a seguir.
+date_birth = df_clients['Data_Nascimento']
 
-# irá fazer o calculo baseado na data atual para determinar a idade de cada cliente e armazenar
-# na coluna 'Idade'.
+## O código a seguir irá fazer o calculo baseado na data atual para determinar a
+#  idade de cada cliente e armazenar na coluna 'idade'.
 today = datetime.today()
-data_clients['Idade'] = (today - data_clients['Data_Nascimento']).dt.days//365
+df_clients['idade'] = (today - df_clients['data_de_nascimento']).dt.days//365
+
+## Pra finalizar, salvando a tabela atualizada em um novo arquivo e mantendo o 
+#  mesmo formato (xlsx).
+df_clients.to_excel('dados/client_data_att.xlsx')
+
